420fd2aadc0b978cfbf1561129bc181495c63913
Liam Scott <vahkiti@maskofdestiny.com>
2018-11-29 17:56:48 -0500

    Initial commit

--- OSITool/Form1.vb
+++ OSITool/Form1.vb
@@ -1,3 +1,28 @@
-﻿Public Class Form1
+﻿Public Class OSITool
+    Private mySubTask As Process
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        mySubTask = Process.Start("ASSEMBLE (ALPHA).bat")
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        mySubTask = Process.Start("DISASSEMBLE (ALPHA).bat")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        mySubTask = Process.Start("ASSEMBLE (LEGACY).bat")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        mySubTask = Process.Start("DISASSEMBLE (LEGACY).bat")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        mySubTask = Process.Start("ASSEMBLE.bat")
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        mySubTask = Process.Start("DISASSEMBLE.bat")
+    End Sub
 
 End Class
